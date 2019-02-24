a0d6d11784ac172ca76c922f11902c6ced7cdf02
jalvarezh1994 <j.alvarezh1994@unitec.edu>
2019-02-24 15:05:09 -0600

    inicio card 26

---
+++ ui/dragCards/dragCards.js
@@ -0,0 +1,9 @@
+
+var dragCards = new Vue({
+  el:'#vue-content',
+  data:{
+    titleText: 'Ordena de manera adecuada las siguientes etiquetas:',
+    card1: '<img',
+    card2: 'src="mono.png"'
+  }
+})
