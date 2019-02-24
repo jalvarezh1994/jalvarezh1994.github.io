c15bc06ed474cfc51ae8d36fd487326666a0be00
Jorge Álvarez <j.alvarezh1994@unitec.edu>
2019-02-24 11:59:07 -0600

    Onsen added

--- src/main.js
+++ src/main.js
@@ -4,6 +4,15 @@ import router from './router'
 
 Vue.config.productionTip = false
 
+// Webpack CSS import
+import 'onsenui/css/onsenui.css';
+import 'onsenui/css/onsen-css-components.css';
+
+// JS import
+import VueOnsen from 'vue-onsenui'; // This imports 'onsenui', so no need to import it separately
+
+Vue.use(VueOnsen); // VueOnsen set here as plugin to VUE. Done automatically if a call to window.Vue exists in the startup code.
+
 new Vue({
   router,
   render: h => h(App)
---
+++ src/views/mainMenu.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="mainMenu">
+    <v-ons-page>
+      <v-ons-toolbar>
+        <div class="center">Title</div>
+      </v-ons-toolbar>
+
+      <p style="text-align: center">
+        <v-ons-button @click="$ons.notification.alert('Hello World!')">Click me!</v-ons-button>
+      </p>
+    </v-ons-page>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "mainMenu"
+};
+</script>
