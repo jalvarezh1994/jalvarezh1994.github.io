0cc152c1349f572a063282d7ad7f72d48d1e6613
jalvarezh1994 <j.alvarezh1994@unitec.edu>
2019-04-08 07:46:48 -0600

    Done

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,24 +1,26 @@
 <template>
-  <div class="page bg-blue">
+  <div class="page bg-gradient">
     <div class="wrapper" style="text-align:center">
+      <img src="/svg/vincet-logo.svg" class="vincet-logo">
       <br>
-      <br>
-      <img src="/logos/vincet.svg" class="vincet-logo">
+      <div class="greeting-text">
+        Bienvenidos a
+        <span>VINCET</span>
+      </div>
       <br>
       <form @submit="login">
-        <span class="login-text">E-Mail</span>
+        <input class="input" type="text" id="email">
         <br>
-        <input class="my-input" type="text" id="email">
+        <input class="input" type="password" id="pass">
         <br>
-        <span class="login-text">Contraseña</span>
-        <br>
-        <input class="my-input" type="password" id="pass">
-        <br>
-        <button type="submit" class="btn my-btn">
-          <span style="margin-left:auto;margin-right:auto;display:flex;font-size:1.5rem">Ingresar</span>
+        <button class="sign-in-btn" type="submit">
+          <span style>Ingresar</span>
         </button>
+        <br>O
+        <div>Registrarse</div>
       </form>
     </div>
+    <footer class="footer"></footer>
   </div>
 </template>
 
@@ -31,7 +33,7 @@ export default {
     login() {
       event.preventDefault();
       const email = document.getElementById("email").value;
-      const password = document.getElementById("email").value;
+      const password = document.getElementById("pass").value;
       axios
         .post(
           "http://172.16.28.180:3000/login/app/movil",
@@ -42,20 +44,53 @@ export default {
         )
         .then(res => {
           if (res.data.error) {
-            //
+            console.log(res.data.error);
           } else {
             window.localStorage.setItem("userId", res.data.user._id);
             console.log(res.data);
             this.$router.push("/mainMenu");
           }
         })
-        .then();
+        .then(err => {
+          if (err) {
+            alert("Hubo un error de autenticación.");
+          }
+        });
     }
   }
 };
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+.bg-gradient {
+  background: linear-gradient(to right, #19b7cb 15%, #39a935 54%);
+}
+.input {
+  width: 80%;
+  border: 3px solid white;
+  height: 3rem;
+  border-radius: 20px;
+}
+.sign-in-btn {
+  width: 81%;
+  background: linear-gradient(to right, #262262 0%, #662d91 100%);
+  border: 3px;
+  border-color: linear-gradient(to right, #262262 0%, #662d91 100%);
+  height: 3rem;
+}
+.sign-in-btn span {
+  margin-left: auto;
+  margin-right: auto;
+  display: flex;
+  font-size: 1.5rem;
+}
+.footer {
+  background: linear-gradient(to right, #262262 0%, #662d91 100%);
+  height: 1rem;
+  width: 100%;
+  bottom: 0px;
+  position: absolute;
+}
 .vincet-logo {
   display: flex;
   margin-left: auto;
@@ -68,19 +103,21 @@ export default {
   margin-bottom: 1rem;
   font-family: Arial, Helvetica, sans-serif;
 }
-.my-btn {
-  display: flex;
-  height: 3rem;
-  width: 70%;
-  margin-left: auto;
-  margin-right: auto;
-  margin-top: 2rem;
-  text-align: center;
+.greeting-text {
+  font-family: "Lato-Regular";
+  font-size: 1.75rem;
+  color: #3d2b57;
+  span {
+    font-family: "Zian";
+    font-size: 1.75rem;
+  }
 }
-.my-input {
-  margin-bottom: 1rem;
-  height: 2rem;
-  width: 100%;
-  font-size: 1.25rem;
+@font-face {
+  font-family: "Lato-Regular";
+  src: url("/fonts/lato/Lato-Regular.ttf");
+}
+@font-face {
+  font-family: "Zian";
+  src: url("/fonts/zian/Zian.ttf");
 }
 </style>
